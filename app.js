b6d013a12baf4f166fc5aeb29130325c6b5d5329
NielMc <niel@codeinstitute.net>
2023-07-25 15:04:40 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+document.getElementById('guessButton').onclick = function() {
+    let numberInput = document.getElementById('numberInput').value;
+    let randomNumber = Math.floor(Math.random() * 10) + 1;
+    let message;
+
+    if (numberInput == randomNumber) {
+        message = "Congratulations! You've guessed correctly.";
+    } else {
+        message = "Sorry, your guess is incorrect. The correct number was " + randomNumber + ".";
+    }
+
+    document.getElementById('message').textContent = message;
+}
