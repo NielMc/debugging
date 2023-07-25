2ae92bacd90d28852dce25487786c9a8340fab5a
NielMc <niel@codeinstitute.net>
2023-07-25 16:44:31 +0100

    add bug

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 document.getElementById('guessButton').onclick = function() {
-    let numberInput = document.getElementById('numberInput').value;
+    let numberInput = document.getElementById('numberImput').value;
     console.log(numberInput);
     let randomNumber = Math.floor(Math.random() * 10) + 1;
     let message;
